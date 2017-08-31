4ce184d91c6abd2fabe77a59c3e58e6705fea299
Joseph Hyatt <jhyatt1017@gmail.com>
2017-08-30 19:58:29 -0700

    Integrated database relationship between topics and blog posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170831020733) do
+ActiveRecord::Schema.define(version: 20170831025324) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -47,6 +47,12 @@ ActiveRecord::Schema.define(version: 20170831020733) do
   create_table "skills", force: :cascade do |t|
     t.string "title"
     t.integer "percent_utilized"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
