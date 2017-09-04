d29ad97f1799241e2d91a3e489061ba00bf040f5
Joseph Hyatt <jhyatt1017@gmail.com>
2017-09-04 14:03:09 -0700

    intergrated styles for blog pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def login_helper style = ""
+  def login_helper style = ''
     if current_user.is_a?(GuestUser)
       (link_to "Register", new_user_registration_path, class: style) +
       " ".html_safe +
@@ -15,8 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
-	
-	def copyright_generator
-		HyattViewTool::Renderer.copyright 'Joseph Hyatt', 'All rights reserved'
-	end
+
+  def copyright_generator
+    HyattViewTool::Renderer.copyright 'Joseph Hyatt', 'All rights reserved'
+  end
 end
