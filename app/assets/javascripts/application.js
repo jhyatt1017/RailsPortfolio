b73b67e8d2a2b20a088583bde24c788e160fe6e0
Joseph Hyatt <jhyatt1017@gmail.com>
2017-09-11 19:36:08 -0700

    move files in application js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,14 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require jquery3
 //= require jquery_ujs
 //= require jquery-ui
 //= require html.sortable
 //= require cocoon
 //= require gritter
-//= require lib/typed
-//= require typed-implementation
 //= require cable
 //= require turbolinks
 //= require_tree .
