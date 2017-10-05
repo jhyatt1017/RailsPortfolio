1f00f1193335b3c8ee42793eb7da8775066eff69
Joseph Hyatt <jhyatt1017@gmail.com>
2017-10-04 20:14:39 -0700

    Updated production rb domain and default url options

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,21 +50,6 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-
-  config.action_mailer.default_url_options = { :host => 'jnh-portfolio.herokuapp.com/' }  
-  config.action_mailer.delivery_method = :smtp  
-  config.action_mailer.perform_deliveries = true  
-  config.action_mailer.raise_delivery_errors = false  
-  config.action_mailer.default :charset => "utf-8"  
-  config.action_mailer.smtp_settings = {  
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "jnh-portfolio.herokuapp.com/",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
-  }
   
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
