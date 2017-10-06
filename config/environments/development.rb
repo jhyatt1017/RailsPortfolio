ea4bbd35f0aa8c51e82a1ac2d68ca98fc24f590d
Joseph Hyatt <jhyatt1017@gmail.com>
2017-10-06 14:56:01 -0700

    working on development file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,6 +50,22 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
+  config.active_record.dump_schema_after_migration = false
+  
+    config.action_mailer.default_url_options = { :host => 'jnh-portfolio.herokuapp.com/' }  
+    config.action_mailer.delivery_method = :smtp  
+    config.action_mailer.perform_deliveries = true  
+    config.action_mailer.raise_delivery_errors = true  
+    config.action_mailer.default :charset => "utf-8"  
+    config.action_mailer.smtp_settings = {  
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: "jnh-portfolio.herokuapp.com/",
+      authentication: "plain",
+      enable_starttls_auto: true,
+      user_name: ENV["GMAIL_USERNAME"],
+      password: ENV["GMAIL_PASSWORD"]
+    }  
   
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
