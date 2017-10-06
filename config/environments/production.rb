6deddd2120ee599fe91f2d4ef1c5914315e12465
Joseph Hyatt <jhyatt1017@gmail.com>
2017-10-06 16:41:37 -0700

    Updated production rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,7 +98,7 @@ Rails.application.configure do
     address: "smtp.gmail.com",
     port: 587,
     domain: "gmail.com",
-    authentication: :login,
+    authentication: "login",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"]
